612b69b829f86118496816cc4a596699f31acdfb
Mark Kremer <mark@5ec.nl>
2013-11-03 19:47:40 +0100

    Setup the Cli logic

--- library/Emphloyer/Cli.php
+++ library/Emphloyer/Cli.php
@@ -2,7 +2,57 @@
 
 namespace Emphloyer;
 
+/**
+ * Cli is the class used by the command line runner to execute input commands.
+ */
 class Cli {
+  protected $lastSignal;
+  protected $pipeline;
+  protected $numberOfEmployees = 0;
+
+  /**
+   * Configure with PHP code from a file.
+   */
+  public function configure($filename) {
+    $forkHooks = new Job\ForkHookChain();
+    require $filename;
+    $this->numberOfEmployees = $numberOfEmployees;
+    $this->pipeline = new Pipeline($pipelineBackend);
+    $this->forkHooks = $forkHooks;
+  }
+
+  /**
+   * Run jobs.
+   */
   public function run() {
+    $this->workshop = new Workshop(new Boss($this->pipeline), $this->forkHooks, $this->numberOfEmployees);
+    
+    declare(ticks = 100);
+    pcntl_signal(\SIGINT, array($this, 'handleSignal'));
+    $this->workshop->run();
+  }
+
+  /**
+   * Clear all jobs from the pipeline.
+   */
+  public function clear() {
+    $this->pipeline->clear();
+  }
+
+  /**
+   * Signal handler.
+   * @param int $signo
+   */
+  public function handleSignal($signo) {
+    switch ($signo) {
+    case \SIGINT:
+      if (!is_null($this->lastSignal) && $this->lastSignal <= (time() - 5)) {
+        $this->workshop->stopNow();
+      } else {
+        $this->lastSignal = time();
+        $this->workshop->stop();
+      }
+      break;
+    }
   }
 }
